dd488e45f278c0c0c421d0ab207169869594fe87
Yannick Majoros <yannick@valuya.be>
2018-02-05 17:17:37 +0100

    test improvements

--- src/test/java/be.valuya.codabox.client/CodaboxClientTest.java
+++ src/test/java/be.valuya.codabox.client/CodaboxClientTest.java
@@ -10,25 +10,23 @@ import be.valuya.codabox.domain.Metadata;
 import be.valuya.codabox.domain.PodClient;
 import be.valuya.codabox.domain.Service;
 import be.valuya.codabox.domain.Software;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import javax.net.ssl.HttpsURLConnection;
-import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URI;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.security.SecureRandom;
-import java.security.cert.X509Certificate;
 import java.util.List;
+import java.util.logging.Logger;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 public class CodaboxClientTest {
+
+    private static final Logger LOGGER = Logger.getLogger(CodaboxClientTest.class.getName());
 
     private CodaboxClient codaboxClient;
 
@@ -49,54 +47,41 @@ public class CodaboxClientTest {
         codaboxClientConfig.setBaseUri(baseUri);
 
         codaboxClient = new CodaboxClient(codaboxClientConfig);
-
-        TrustManager[] trustAllCerts = new TrustManager[]{new X509TrustManager() {
-            public X509Certificate[] getAcceptedIssuers() {
-                return null;
-            }
-
-            public void checkClientTrusted(X509Certificate[] certs, String authType) {
-            }
-
-            public void checkServerTrusted(X509Certificate[] certs, String authType) {
-            }
-        }};
-
-// Install the all-trusting trust manager
-        try {
-            SSLContext sc = SSLContext.getInstance("TLS");
-            sc.init(null, trustAllCerts, new SecureRandom());
-            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
-        } catch (Exception e) {
-            ;
-        }
     }
 
     @Test
     public void testGetPodClient() {
         PodClient podClient = codaboxClient.getPodClient();
         Integer fetchDelay = podClient.getFetchDelay();
-
-        System.out.println("fetchDelay : " + fetchDelay);
-
         AllowedFormats allowedFormats = podClient.getAllowedFormats();
-        printAllowedFormats(allowedFormats);
-        System.out.println("allowedFormats : " + allowedFormats);
-
         List<FeedClient> feedClients = podClient.getFeedClients();
+
+        List<String> allowedFormatsCoda = allowedFormats.getCoda();
+        Assertions.assertFalse(allowedFormatsCoda.isEmpty(), "should have at least one allowed format for coda");
+
+        LOGGER.info("fetchDelay : " + fetchDelay);
+
+        printAllowedFormats(allowedFormats);
+
         feedClients.forEach(this::printFeedClient);
     }
 
     @Test
     public void testGetFiduciaries() {
         List<Fiduciary> fiduciaries = codaboxClient.getFiduciaries();
+
+        Assertions.assertFalse(fiduciaries.isEmpty(), "should find at list one fiduciary");
+
         fiduciaries.forEach(this::printFiduciary);
     }
 
     @Test
     public void testGetFeed() {
         PodClient podClient = codaboxClient.getPodClient();
+
         List<FeedClient> feedClients = podClient.getFeedClients();
+        Assertions.assertFalse(feedClients.isEmpty(), "should have at least one feed client");
+
         feedClients.stream()
                 .findFirst()
                 .ifPresent(this::testGetFeed);
@@ -106,44 +91,19 @@ public class CodaboxClientTest {
     public void testDownload() {
         PodClient podClient = codaboxClient.getPodClient();
         List<FeedClient> feedClients = podClient.getFeedClients();
+
         feedClients.stream()
                 .findFirst()
-                .ifPresent(this::downloadFromFeed);
+                .ifPresent(this::testDownloadFromFeed);
     }
 
-    private void printFiduciary(Fiduciary fiduciary) {
-        String id = fiduciary.getId();
-        String name = fiduciary.getName();
-        String contactEmail = fiduciary.getContactEmail();
-        String contactName = fiduciary.getContactName();
-        String language = fiduciary.getLanguage();
-        Boolean allowsClientDelivery = fiduciary.getAllowsClientDelivery();
-        Boolean allowsDoccleDelivery = fiduciary.getAllowsDoccleDelivery();
-        Boolean allowsSocialMandates = fiduciary.getAllowsSocialMandates();
-        LegalEntity legalEntity = fiduciary.getLegalEntity();
-        List<Service> services = fiduciary.getServices();
-        Software software = fiduciary.getSoftware();
-        Boolean delegatesMandateDelivery = fiduciary.getDelegatesMandateDelivery();
-        String websiteUrl = fiduciary.getWebsiteUrl();
-
-        System.out.println("id: " + id);
-        System.out.println("name: " + name);
-        System.out.println("contactEmail: " + contactEmail);
-        System.out.println("contactName: " + contactName);
-        System.out.println("language: " + language);
-        System.out.println("allowsClientDelivery: " + allowsClientDelivery);
-        System.out.println("allowsDoccleDelivery: " + allowsDoccleDelivery);
-        System.out.println("allowsSocialMandates: " + allowsSocialMandates);
-        System.out.println("legalEntity: " + legalEntity);
-        System.out.println("services: " + services);
-        System.out.println("software: " + software);
-        System.out.println("delegatesMandateDelivery: " + delegatesMandateDelivery);
-        System.out.println("websiteUrl: " + websiteUrl);
-    }
-
-    private void downloadFromFeed(FeedClient feedClient) {
+    private void testDownloadFromFeed(FeedClient feedClient) {
         Feed feed = codaboxClient.getFeed(feedClient);
-        feed.getFeedEntries()
+        List<FeedEntry> feedEntries = feed.getFeedEntries();
+
+        Assertions.assertFalse(feedEntries.isEmpty(), "should have at least one feed entry");
+
+        feedEntries
                 .stream()
                 .findFirst()
                 .ifPresent(this::downloadFeedEntry);
@@ -164,7 +124,44 @@ public class CodaboxClientTest {
 
     private void testGetFeed(FeedClient feedClient) {
         Feed feed = codaboxClient.getFeed(feedClient);
+        Assertions.assertNotNull(feed, "should get some feed");
+
+        Integer feedId = feed.getId();
+        Assertions.assertNotNull(feedId, "feed should have an id");
+
         printFeed(feed);
+    }
+
+    private void printFiduciary(Fiduciary fiduciary) {
+        String id = fiduciary.getId();
+        String name = fiduciary.getName();
+        String contactEmail = fiduciary.getContactEmail();
+        String contactName = fiduciary.getContactName();
+        String language = fiduciary.getLanguage();
+        Boolean allowsClientDelivery = fiduciary.getAllowsClientDelivery();
+        Boolean allowsDoccleDelivery = fiduciary.getAllowsDoccleDelivery();
+        Boolean allowsSocialMandates = fiduciary.getAllowsSocialMandates();
+        LegalEntity legalEntity = fiduciary.getLegalEntity();
+        List<Service> services = fiduciary.getServices();
+        Software software = fiduciary.getSoftware();
+        Boolean delegatesMandateDelivery = fiduciary.getDelegatesMandateDelivery();
+        String websiteUrl = fiduciary.getWebsiteUrl();
+
+        LOGGER.info("id: " + id);
+        LOGGER.info("name: " + name);
+        LOGGER.info("contactEmail: " + contactEmail);
+        LOGGER.info("contactName: " + contactName);
+        LOGGER.info("language: " + language);
+        LOGGER.info("allowsClientDelivery: " + allowsClientDelivery);
+        LOGGER.info("allowsDoccleDelivery: " + allowsDoccleDelivery);
+        LOGGER.info("allowsSocialMandates: " + allowsSocialMandates);
+        LOGGER.info("legalEntity: " + legalEntity);
+        LOGGER.info("software: " + software);
+        LOGGER.info("delegatesMandateDelivery: " + delegatesMandateDelivery);
+        LOGGER.info("websiteUrl: " + websiteUrl);
+
+        LOGGER.info("services: ");
+        services.forEach(this::printService);
     }
 
     private void printFeed(Feed feed) {
@@ -172,17 +169,28 @@ public class CodaboxClientTest {
         String type = feed.getType();
         List<FeedEntry> feedEntries = feed.getFeedEntries();
 
-        System.out.println("id: " + id);
-        System.out.println("type: " + type);
+        LOGGER.info("id: " + id);
+        LOGGER.info("type: " + type);
         feedEntries.forEach(this::printFeedEntry);
+    }
+
+    private void printService(Service service) {
+        String id = service.getId();
+        String state = service.getState();
+        String type = service.getType();
+
+        LOGGER.info("  id: " + id);
+        LOGGER.info("  state: " + state);
+        LOGGER.info("  type: " + type);
     }
 
     private void printFeedEntry(FeedEntry feedEntry) {
         String documentModel = feedEntry.getDocumentModel();
         String feedIndex = feedEntry.getFeedIndex();
         Metadata metadata = feedEntry.getMetadata();
-        System.out.println("  documentModel: " + documentModel);
-        System.out.println("  feedIndex: " + feedIndex);
+
+        LOGGER.info("  documentModel: " + documentModel);
+        LOGGER.info("  feedIndex: " + feedIndex);
         printMetadata(metadata);
     }
 
@@ -195,14 +203,15 @@ public class CodaboxClientTest {
         Integer lastStatementNumber = metadata.getLastStatementNumber();
         Integer movementCount = metadata.getMovementCount();
         String newBalanceDate = metadata.getNewBalanceDate();
-        System.out.println("    bankId: " + bankId);
-        System.out.println("    currency: " + currency);
-        System.out.println("    extensionZone: " + extensionZone);
-        System.out.println("    firstStatementNumber: " + firstStatementNumber);
-        System.out.println("    iban: " + iban);
-        System.out.println("    lastStatementNumber: " + lastStatementNumber);
-        System.out.println("    movementCount: " + movementCount);
-        System.out.println("    newBalanceDate: " + newBalanceDate);
+
+        LOGGER.info("    bankId: " + bankId);
+        LOGGER.info("    currency: " + currency);
+        LOGGER.info("    extensionZone: " + extensionZone);
+        LOGGER.info("    firstStatementNumber: " + firstStatementNumber);
+        LOGGER.info("    iban: " + iban);
+        LOGGER.info("    lastStatementNumber: " + lastStatementNumber);
+        LOGGER.info("    movementCount: " + movementCount);
+        LOGGER.info("    newBalanceDate: " + newBalanceDate);
     }
 
     private void printAllowedFormats(AllowedFormats allowedFormats) {
@@ -213,12 +222,13 @@ public class CodaboxClientTest {
         List<String> sodaFormats = allowedFormats.getSoda();
 
         Collector<CharSequence, ?, String> hyphenJoiningCollector = Collectors.joining(" - ");
-        System.out.println("allowedFormats:");
-        System.out.println("  expenseFormats : " + expenseFormats.stream().collect(hyphenJoiningCollector));
-        System.out.println("  purchaseInvoiceFormats : " + purchaseInvoiceFormats.stream().collect(hyphenJoiningCollector));
-        System.out.println("  salesInvoiceFormats : " + salesInvoiceFormats.stream().collect(hyphenJoiningCollector));
-        System.out.println("  codaFormats : " + codaFormats.stream().collect(hyphenJoiningCollector));
-        System.out.println("  sodaFormats : " + sodaFormats.stream().collect(hyphenJoiningCollector));
+
+        LOGGER.info("allowedFormats:");
+        LOGGER.info("  expenseFormats : " + expenseFormats.stream().collect(hyphenJoiningCollector));
+        LOGGER.info("  purchaseInvoiceFormats : " + purchaseInvoiceFormats.stream().collect(hyphenJoiningCollector));
+        LOGGER.info("  salesInvoiceFormats : " + salesInvoiceFormats.stream().collect(hyphenJoiningCollector));
+        LOGGER.info("  codaFormats : " + codaFormats.stream().collect(hyphenJoiningCollector));
+        LOGGER.info("  sodaFormats : " + sodaFormats.stream().collect(hyphenJoiningCollector));
     }
 
     private void printFeedClient(FeedClient feedClient) {
@@ -229,11 +239,11 @@ public class CodaboxClientTest {
         Object deliveryConfig = feedClient.getDeliveryConfig();
         String targetRootState = feedClient.getTargetRootState();
 
-        System.out.println("  clientId : " + clientId);
-        System.out.println("  clientCode : " + clientCode);
-        System.out.println("  clientName : " + clientName);
-        System.out.println("  fiduciaryId : " + fiduciaryId);
-        System.out.println("  deliveryConfig : " + deliveryConfig);
-        System.out.println("  targetRootState : " + targetRootState);
+        LOGGER.info("  clientId : " + clientId);
+        LOGGER.info("  clientCode : " + clientCode);
+        LOGGER.info("  clientName : " + clientName);
+        LOGGER.info("  fiduciaryId : " + fiduciaryId);
+        LOGGER.info("  deliveryConfig : " + deliveryConfig);
+        LOGGER.info("  targetRootState : " + targetRootState);
     }
 }
